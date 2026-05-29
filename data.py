b8907433fc737370f64861de7594a002bf209611
leuitikon <lucasrez777@gmail.com>
2026-05-29 20:09:46 -0300

    Add files via upload

---
+++ data.py
@@ -0,0 +1,83 @@
+question_data = [
+    {
+        "type": "boolean",
+        "difficulty": "easy",
+        "category": "Entertainment: Japanese Anime & Manga",
+        "question": "In Pokémon, Ash's Pikachu refuses to go into a pokeball.",
+        "correct_answer": "True",
+        "incorrect_answers": ["False"]
+    },
+    {
+        "type": "boolean",
+        "difficulty": "easy",
+        "category": "Entertainment: Japanese Anime & Manga",
+        "question": "In the \"To Love-Ru\" series, Golden Darkness is sent to kill Lala Deviluke.",
+        "correct_answer": "False",
+        "incorrect_answers": ["True"]
+    },
+    {
+        "type": "boolean",
+        "difficulty": "easy",
+        "category": "Entertainment: Japanese Anime & Manga",
+        "question": "In Miss Kobayashi's Dragon Maid, Tohru is a member of the harmony faction.",
+        "correct_answer": "False",
+        "incorrect_answers": ["True"]
+    },
+    {
+        "type": "boolean",
+        "difficulty": "easy",
+        "category": "Entertainment: Japanese Anime & Manga",
+        "question": "In the 1988 film \"Akira\", Tetsuo ends up destroying Tokyo.",
+        "correct_answer": "True",
+        "incorrect_answers": ["False"]
+    },
+    {
+        "type": "boolean",
+        "difficulty": "easy",
+        "category": "Entertainment: Japanese Anime & Manga",
+        "question": "In the anime \"Naruto\", the main character is a female.",
+        "correct_answer": "False",
+        "incorrect_answers": ["True"]
+    },
+    {
+        "type": "boolean",
+        "difficulty": "easy",
+        "category": "Entertainment: Japanese Anime & Manga",
+        "question": "In the \"Toaru Kagaku no Railgun\" anime, espers can only reach a maximum of level 6 in their abilities.",
+        "correct_answer": "False",
+        "incorrect_answers": ["True"]
+    },
+    {
+        "type": "boolean",
+        "difficulty": "easy",
+        "category": "Entertainment: Japanese Anime & Manga",
+        "question": "The name of the attack \"Kamehameha\" in Dragon Ball Z was named after a famous king of Hawaii.",
+        "correct_answer": "True",
+        "incorrect_answers": ["False"]
+    },
+    {
+        "type": "boolean",
+        "difficulty": "easy",
+        "category": "Entertainment: Japanese Anime & Manga",
+        "question": "In the \"Melancholy of Haruhi Suzumiya\" series, the narrator goes by the nickname Kyon.",
+        "correct_answer": "True",
+        "incorrect_answers": ["False"]
+    },
+    {
+        "type": "boolean",
+        "difficulty": "easy",
+        "category": "Entertainment: Japanese Anime & Manga",
+        "question": "In Chobits, Hideki found Chii in his apartment.",
+        "correct_answer": "False",
+        "incorrect_answers": ["True"]
+    },
+    {
+        "type": "boolean",
+        "difficulty": "easy",
+        "category": "Entertainment: Japanese Anime & Manga",
+        "question": "Clefairy was intended to be Ash's starting Pokémon in the pilot episode of the cartoon.",
+        "correct_answer": "True",
+        "incorrect_answers": ["False"]
+    }
+]
+
